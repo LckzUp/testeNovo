de20efabfb396b556acc992d29fd86e48b43f18c
LckzUp <lucas.pinto17@escola.pr.gov.br>
2025-05-16 11:27:58 -0300

    edentação

--- src/main/java/com/lckzup/modelo/Zezo.java
+++ src/main/java/com/lckzup/modelo/Zezo.java
@@ -46,31 +46,31 @@ public class Zezo {
     @Override
     public boolean equals(Object obj) {
         if (this == obj)
-        return true;
+            return true;
         if (obj == null)
-        return false;
+            return false;
         if (getClass() != obj.getClass())
-        return false;
+            return false;
         Zezo other = (Zezo) obj;
         if (nome == null) {
             if (other.nome != null)
-            return false;
+                return false;
         } else if (!nome.equals(other.nome))
-        return false;
+            return false;
         if (idade != other.idade)
-        return false;
-        return true;
+            return false;
+                return true;
     }
     
     @Override
     public int hashCode() {
         final int prime = 31;
-        int result = 1;
+            int result = 1;
         result = prime * result + ((nome == null) ? 0 : nome.hashCode());
         result = prime * result + idade;
-        return result;
+            return result;
     }
 
-    
+
     
 }
